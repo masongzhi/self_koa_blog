4dd822c02e53d53f83090327fe92a0d2a56607ce
masongzhi <511071161@qq.com>
2018-06-28 10:28:56 +0800

    删除不必要test文件

--- test/service/App.getStartBannerPicture.test.js
+++
@@ -1,9 +0,0 @@
-const AppBsnService = require('../../app/service/AppBsnService');
-
-describe('应该获取tabIcons', function () {
-  it('检查应该正常', async function () {
-    let result = await AppBsnService.getStartBannerPicture();
-    result.tabIcons.length.should.be.equal(4);
-    result.tabIcons[0].unselect.should.be.equal('mainaaaaaaaaa');
-  });
-});
